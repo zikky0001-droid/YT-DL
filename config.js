2f61d0223a4007b3bfff8ad11a4294aa16456951
ZIKKY STAR <zikkystar0001@gmail.com>
2025-11-30 16:51:13 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export default {
-  TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
-  API_MARKET_KEY: process.env.API_MARKET_KEY,
-  PORT: process.env.PORT || 3000,
-  TEMP_DIR: process.env.TEMP_DIR || "/tmp"
-};
